b8724b45adc2b76cc95f54d9b8ec7d80ffd6c125
Angela Raymond <angela.raymond@ntconcepts.com>
2020-03-03 14:08:40 -0500

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import WhiteLogo from '../../assets/images/white-logo.png';
+import BlackLogo from '../../assets/images/black-logo.png';
+
+const Header = (props) => {
+	return(
+		<div id={'header'}>
+			<header className='main-header'>
+				<img src={WhiteLogo}  alt='Black Logo'/>
+				<h2>Sample Test Jest</h2>
+			</header>
+		</div>
+	)
+};
+
+export default Header;
